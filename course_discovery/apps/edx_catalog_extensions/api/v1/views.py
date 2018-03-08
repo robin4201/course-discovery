6f0a811e5203f9b58a546a428522379e70cbfb97
christopher lee <clee@edx.org>
2018-03-08 15:03:45 -0500

    cache

--- course_discovery/apps/edx_catalog_extensions/api/v1/views.py
+++ course_discovery/apps/edx_catalog_extensions/api/v1/views.py
@@ -1,6 +1,7 @@
 from course_discovery.apps.api.v1.views.search import AggregateSearchViewSet
 from course_discovery.apps.edx_catalog_extensions.api.serializers import DistinctCountsAggregateFacetSearchSerializer
 from course_discovery.apps.edx_haystack_extensions.distinct_counts.query import DistinctCountsSearchQuerySet
+from functools import lru_cache
 
 
 class DistinctCountsAggregateSearchViewSet(AggregateSearchViewSet):
@@ -9,6 +10,7 @@ class DistinctCountsAggregateSearchViewSet(AggregateSearchViewSet):
     # Custom serializer that includes distinct hit and facet counts.
     facet_serializer_class = DistinctCountsAggregateFacetSearchSerializer
 
+    @lru_cache(maxsize=None)
     def get_queryset(self, *args, **kwargs):  # pylint: disable=arguments-differ
         """ Return the base Queryset to use to build up the search query."""
         queryset = super(DistinctCountsAggregateSearchViewSet, self).get_queryset(*args, **kwargs)
